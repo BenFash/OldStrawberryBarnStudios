96ec77da953135c1903537bdf836c4720411dfa4
Ben Fashan <benfashan71@msn.com>
2024-05-30 23:34:55 +0000

    ADD: style to pubs container + media queried all area guide containers

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,21 +1,51 @@
 console.log('script.js loaded');
 
 (function () {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init({
-        publicKey: "tDN0tUMP56E9wc3Dc",
-    });
+    emailjs.init("tDN0tUMP56E9wc3Dc");
 })();
 
 window.onload = function () {
     document.getElementById('res-form').addEventListener('submit', function (event) {
         event.preventDefault();
-        // these IDs from the previous steps
-        emailjs.sendForm('reservation_noti', this)
+
+        const form = this;
+
+        // Send email with EmailJS
+        emailjs.sendForm('service_2kin33p', 'reservation_noti', form)
             .then(() => {
-                console.log('SUCCESS!');
+                console.log('Email sent successfully!');
+
+                // AJAX request to submit form data to the server
+                const xhr = new XMLHttpRequest();
+                xhr.open("POST", form.action, true);
+                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        console.log('Response received:', xhr.responseText); // Log the response text
+                        if (xhr.status === 200) {
+                            try {
+                                const response = JSON.parse(xhr.responseText);
+                                if (response.redirect_url) {
+                                    window.location.href = response.redirect_url;
+                                } else {
+                                    console.log('No redirect URL found in the response');
+                                }
+                            } catch (e) {
+                                console.error('Failed to parse JSON response:', e);
+                            }
+                        } else {
+                            console.error('Server responded with status:', xhr.status);
+                            console.error('Response text:', xhr.responseText);
+                        }
+                    }
+                };
+                const formData = new FormData(form);
+                const data = new URLSearchParams(formData).toString();
+                xhr.send(data);
             }, (error) => {
-                console.log('FAILED...', error);
+                console.log('Email sending failed...', error);
+                alert('Failed to send reservation request. Please try again later.');
             });
     });
-}
+};
