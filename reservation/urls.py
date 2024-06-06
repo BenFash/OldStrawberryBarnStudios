9140631863ccf4cc18d8094517e35911f9264e55
Ben Fashan <benfashan71@msn.com>
2024-06-06 17:00:20 +0000

    FIX: ref to rooms changed to studio in navbar, rooms.html

--- reservation/urls.py
+++ reservation/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.RoomsView, name='rooms'),
+    path('', views.RoomsView, name='studios'),
     path('<int:pk>/', views.RoomDetailView, name='room_detail'),
     path('reservation/<int:pk>/', views.ReservationView, name='reservation'),    
 ]
